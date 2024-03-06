c6b2ab0d594d41325f9624f390923be2d81b9cb0
Dylan Wicker <dylanwicker27@gmail.com>
2024-03-05 19:36:06 -0500

    loading pdf

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World")
+import axios from 'axios';
+
+async function loadPdfFromUrl(url: string): Promise<Buffer> {
+    const response = await axios.get(url, {
+        responseType: 'arraybuffer',
+    });
+    return Buffer.from(response.data);
+}
+
+async function main ({
+    paperUrl,
+    name, 
+    pagesToDelete,
+
+}: {
+    paperUrl: string,
+    name: string,
+    pagesToDelete?: number[]
+}) {
+    if (!paperUrl.endsWith("pdf")) {
+        throw new Error("Not a PDF");
+    }
+    const pdfAsBuffer = await loadPdfFromUrl(paperUrl)
+}
