1510e6cd478b6710593aba0f4f0c9e0e422a284c
Dylan Wicker <dylanwicker27@gmail.com>
2024-03-06 07:52:41 -0500

    interpretting and reading file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,41 @@
 import axios from 'axios';
+import { PDFDocument } from 'pdf-lib';
+import { Document } from "langchain/document";
+import { writeFile, unlink } from 'fs/promises';
+import { UnstructuredLoader } from 'langchain/document_loaders/fs/unstructured';
+
+async function deletePages(pdf: Buffer, pagesToDelete: number[]): Promise<Buffer> {
+    const pdfDoc = await PDFDocument.load(pdf);
+    let numToOffsetBy =1;
+    for (const pageNum of pagesToDelete) {
+        pdfDoc.removePage(pageNum - numToOffsetBy);
+        numToOffsetBy++;
+    }
+    const pdfBytes = await pdfDoc.save();
+    return Buffer.from(pdfBytes);
+}
 
 async function loadPdfFromUrl(url: string): Promise<Buffer> {
     const response = await axios.get(url, {
         responseType: 'arraybuffer',
     });
-    return Buffer.from(response.data);
+    return response.data;
+}
+
+async function convertPdfToDocuments(pdf: Buffer): Promise<Array<Document>> {
+    if (!process.env.UNSTRUCTURED_API_KEY) {
+        throw new Error("Missing UNSTRUCTURED_API_KEY") 
+    }
+    const randomName = Math.random().toString(36).substring(7);
+    await writeFile(process.env.UNSTRUCTURED_API_KEY, pdf, 'binary');
+    const loader = new UnstructuredLoader(`pdfs/${randomName}.pdf`, {
+        apiKey: process.env.UNSTRUCTURED_API_KEY,
+        strategy: 'hi_res',
+
+    })
+    const documents = await loader.load();
+    await unlink(`pdfs/${randomName}.pdf`);
+    return documents;
 }
 
 async function main ({
@@ -20,5 +51,8 @@ async function main ({
     if (!paperUrl.endsWith("pdf")) {
         throw new Error("Not a PDF");
     }
-    const pdfAsBuffer = await loadPdfFromUrl(paperUrl)
+    let pdfAsBuffer = await loadPdfFromUrl(paperUrl)
+        if (pagesToDelete && pagesToDelete.length > 0) {
+            pdfAsBuffer = await deletePages(pdfAsBuffer, pagesToDelete);
+        } 
 }
